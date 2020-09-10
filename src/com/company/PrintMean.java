2a25d47ec40ae7cdc1daf1999eab85cf9ec73cc2
Besim <Besim.musliu@student.kyh.se>
2020-09-10 14:06:41 +0200

    Added comment

--- src/com/company/PrintMean.java
+++ src/com/company/PrintMean.java
@@ -3,18 +3,17 @@ package com.company;
 public class PrintMean {
 
     public static void main(String[] args) {
-        //Heltalsdivision (skriver ut int istället för double)
-        //detta är en test hehehehehehehehe
         System.out.println("Mean of 1, 1, 3, 3: " + meanCalc(1.0,1.0,3.0,3.0));
         System.out.println("Mean of 1, 2, 3, 4: " + meanCalc(1.0,2.0,3.0,4.0));
         System.out.println("Mean of 1, -2, 3, -4: " + meanCalc(1.0,-2.0,3.0,-4.0));
         System.out.println("Mean of -1, -11, 24, 2: " + meanCalc(-1.0,-11.0,24.0,2.0));
     }
     public static String meanCalc(double a, double b, double c, double d){
-        //gör en sträng av "mean"
         double mean = (a + b + c + d) / 4;
         String meanString = String.valueOf(mean);
-        //en majseeeeeeees
         return meanString;
+
+
+        // ADDED COMMENT
     }
 }
