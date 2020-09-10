1ab8108c02777a2001e17fc32d254a17f1753c10
Besim <Besim.musliu@student.kyh.se>
2020-09-10 14:40:44 +0200

    COmmit

--- src/com/company/PrintMean.java
+++ src/com/company/PrintMean.java
@@ -3,7 +3,7 @@ package com.company;
 public class PrintMean {
 
     public static void main(String[] args) {
-        System.out.println("Mean of 1, 1, 3, 3: " + meanCalc(1.0,1.0,3.0,3.0));
+        System.out.println("Mean of 11, 11, 33, 33: " + meanCalc(11.0,11.0,31.0,31.0));
         System.out.println("Mean of 1, 2, 3, 4: " + meanCalc(1.0,2.0,3.0,4.0));
         System.out.println("Mean of 1, -2, 3, -4: " + meanCalc(1.0,-2.0,3.0,-4.0));
         System.out.println("Mean of -1, -11, 24, 2: " + meanCalc(-1.0,-11.0,24.0,2.0));
