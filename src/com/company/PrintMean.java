1c1ffeb8593085f4fe108ef5a10551a4616399a0
Besim <Besim.musliu@student.kyh.se>
2020-09-10 14:27:39 +0200

    Added mean of -11 -111 244 22

--- src/com/company/PrintMean.java
+++ src/com/company/PrintMean.java
@@ -7,10 +7,13 @@ public class PrintMean {
         System.out.println("Mean of 1, 2, 3, 4: " + meanCalc(1.0,2.0,3.0,4.0));
         System.out.println("Mean of 1, -2, 3, -4: " + meanCalc(1.0,-2.0,3.0,-4.0));
         System.out.println("Mean of -1, -11, 24, 2: " + meanCalc(-1.0,-11.0,24.0,2.0));
+        System.out.println("Mean of -11, -111, 244, 22: " + meanCalc(-11.0,-111.0,244.0,22.0));
     }
     public static String meanCalc(double a, double b, double c, double d){
         double mean = (a + b + c + d) / 4;
         String meanString = String.valueOf(mean);
         return meanString;
+
+
     }
 }
